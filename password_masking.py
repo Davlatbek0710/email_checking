93ffabf78b186228c30488aa24a8e7b9f8be5009
Davlatbek Kobiljonov Sherzodovich <117518580+Davlatbek0710@users.noreply.github.com>
2023-07-20 06:41:32 -0400

    Add files via upload

---
+++ password_masking.py
@@ -0,0 +1,11 @@
+code = "absd382@#fj$nw"
+
+
+# print(code[-4::])
+# edited =  len(code[:-4:])*'#'+ code[-4::]
+# print(edited)
+
+# edited = ''
+# for i in range(len(code) - 4):
+#     edited += '#'
+# print(edited + code[-4:])
