a2a8044ed83795304d3f2aaa2483cfaa2fdf67a5
Davlatbek Kobiljonov Sherzodovich <117518580+Davlatbek0710@users.noreply.github.com>
2023-07-22 12:48:02 -0400

    Update password_masking.py

--- password_masking.py
+++ password_masking.py
@@ -1,11 +1,42 @@
 code = "absd382@#fj$nw"
+#input > absd382@#fj$nw
+#output >  ##############j$nw
+
+print(code[-4::])
+edited =  len(code[:-4:])*'#'+ code[-4::]
+print(edited)
+
+edited = ''
+for i in range(len(code) - 4):
+    edited += '#'
+print(edited + code[-4:])
 
 
-# print(code[-4::])
-# edited =  len(code[:-4:])*'#'+ code[-4::]
-# print(edited)
 
-# edited = ''
-# for i in range(len(code) - 4):
-#     edited += '#'
-# print(edited + code[-4:])
+
+
+
+
+#Problem 
+
+'''
+Move the first letter of each word to the end of it, 
+then add "ay" to the end of the word. Leave punctuation marks untouched.
+'''
+
+# instead of this>
+text = 'Pig latin is cool  ?'
+print(' '.join(x[1:]+x[0]+'ay' for x in text.split()))
+res = []
+for i in text.split():
+    if i != '!' and i !='?':
+        res.append(i[1:]+i[0]+'ay')
+    else:
+        res.append(i)
+print(' '.join(res))
+
+#write this>
+def text(sentance):
+    return  ' '.join(x[1:]+x[0]+'ay' if x.isalpha() else x for x in sentance.split())
+
+print(text('Today we are gonna go to the beatch '))
